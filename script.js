7d114ea429b28b98668c5ab2f240cca5bb05fdd7
Titrat Cristina <31698958+titratcristina@users.noreply.github.com>
2018-04-09 16:05:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,27 +59,27 @@ var gg2 = new JustGage({
 
 function update() {
     console.log('se actualizeaza...');
-    $('#temp_s').removeClass('w3-red');
-    $('#temp_s').removeClass('w3-green');
-    $('#temp_s').addClass('w3-blue');
+    $('#temp_s').removeClass('alert-danger');  
+    $('#temp_s').removeClass('alert-success');   
+    $('#temp_s').addClass('alert-warning');       
     $('#temp_s').text('Actualizare...');
-    $('#umid_s').removeClass('w3-red');
-    $('#umid_s').removeClass('w3-green');
-    $('#umid_s').addClass('w3-blue');
+    $('#umid_s').removeClass('alert-danger'); 
+    $('#umid_s').removeClass('alert-success');
+    $('#umid_s').addClass('alert-warning');   
     $('#umid_s').text('Actualizare...');
 
     $.get(location.protocol+'//'+location.host+'/temp', function (data) {
         gg1.refresh(data);
     }).done(function () {
-        $('#temp_s').removeClass('w3-blue');
-        $('#temp_s').removeClass('w3-red');
-        $('#temp_s').addClass('w3-green');
+        $('#temp_s').removeClass('alert-warning');
+        $('#temp_s').removeClass('alert-danger');
+        $('#temp_s').addClass('alert-success');
         $('#temp_s').text('Actualizat cu succes');
         console.log('OK 4 temp');
     }).fail(function () {
-        $('#temp_s').removeClass('w3-green');
-        $('#temp_s').removeClass('w3-blue');
-        $('#temp_s').addClass('w3-red');
+        $('#temp_s').removeClass('alert-success');
+        $('#temp_s').removeClass('alert-warning');
+        $('#temp_s').addClass('alert-danger');
         $('#temp_s').text('Eroare');
         console.log('ERROR 4 temp');
     }).always(function () {});
@@ -88,15 +88,15 @@ function update() {
         console.log('OK 4 umid');
         gg2.refresh(data);
     }).done(function () {
-        $('#umid_s').removeClass('w3-red');
-        $('#umid_s').removeClass('w3-blue');
-        $('#umid_s').addClass('w3-green');
+        $('#umid_s').removeClass('alert-danger');
+        $('#umid_s').removeClass('alert-warning');
+        $('#umid_s').addClass('alert-success');
         $('#umid_s').text('Actualizat cu succes');
         console.log('OK 4 umid');
     }).fail(function () {
-        $('#umid_s').removeClass('w3-green');
-        $('#umid_s').removeClass('w3-blue');
-        $('#umid_s').addClass('w3-red');
+        $('#umid_s').removeClass('alert-success');
+        $('#umid_s').removeClass('alert-warning');
+        $('#umid_s').addClass('alert-danger');
         $('#umid_s').text('Eroare');
         console.log('ERROR 4 umid');
     }).always(function () {});
